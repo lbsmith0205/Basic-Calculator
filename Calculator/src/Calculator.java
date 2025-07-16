4477e80529aa3befc6dba93e0f411091d220ee84
lbsmith0205 <lb.smith0205@gmail.com>
2025-07-16 02:11:35 +0100

    Created Button 1

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -4,21 +4,32 @@ import java.awt.event.*;
 class Calculator extends WindowAdapter implements ActionListener{
     Frame f;
     Label l;
+    Button b1;
 
 
     Calculator(){
-        f = new Frame("Lukes Calculator");
+        f = new Frame("Currency Calculator");
 
         //Creating Base GUI for calculator
         l = new Label();
         l.setBackground(Color.GRAY);
-        l.setBounds(100,100,100,100);
+        l.setBounds(50,50,260,60);
+
+        b1 = new Button("1");
+            b1.setBounds(50,340,50,50);
+
+        b1.addActionListener(this);
 
         f.addWindowListener(this);
 
-        f.setSize(400,500);
+        //adding components to frame
+        f.add(l);
+        f.add(b1);
+
+        f.setSize(360,500);
         f.setLayout(null);
         f.setVisible(true);
+
     }
 
     // To close window
@@ -27,6 +38,14 @@ class Calculator extends WindowAdapter implements ActionListener{
     }
 
     public void actionPerformed(ActionEvent e) {
+        String z,zt;
+
+
+        if(e.getSource()==b1){
+            zt = l.getText();
+            z = zt +"1";
+            l.setText(z);
+        }
 
     }
 }
